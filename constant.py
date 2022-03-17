04ee64db7efe7d2f2145392c2b1f89b9a3efcc74
lLeshii <hasanovtimofeika@gmail.com>
2022-03-17 20:02:10 +0300

    function update

--- constant.py
+++ constant.py
@@ -1,6 +1,6 @@
-replace_dict = {"\n": " ", "'ll": " will", "'m": " am", "'s": " is", "'re": " are",
-                "won't": "would not", "n't": " not", "Mr.": "Mister ", "Mrs.": "Misses "}
-check_list = [",", ";", "\"", "\'", ":", "(", ")", "?", "!"]
-end_list = [";", "?", "!"]
-n_symb = 4
-m_symb = 10
+REPLACE_DICT = {"\n": " ", "'ll": " will", "'m": " am", "'s": " is", "'re": " are",
+                "won't": "would not", "n't": " not", "Mr.": "Mister ", "Mrs.": "Misses ", "...": ".", "— ": ""}
+CHECK_LIST = [",", ";", "\"", "\'", ":", "(", ")", "?", "!"]
+END_LIST = [";", "?", "!"]
+N_SYMB = 4
+M_SYMB = 10
--- lab1_isp.py
+++ lab1_isp.py
@@ -2,32 +2,37 @@ import statistics
 import constant
 
 
+def abriav_checking(word, posit):
+    value = False
+
+    if word[posit] == "." and (word[posit - 1].islower() or word[posit - 1].isnumeric()):
+        value = True
+
+    return value
+
+
 def clean_word(old_word):
+    old_word = old_word.lower()
     length = len(old_word)
     end_sent = False
     symb_pos = 0
 
     for i in old_word:
-        if symb_pos <= length - 1:
-            if i in constant.check_list:
-                if i in constant.end_list:
-                    end_sent = True
-                length -= 1
-                old_word = old_word[:symb_pos] + old_word[symb_pos + 1:]
-            elif (i == "." and old_word[symb_pos - 1].islower()) or \
-                    (i == "." and old_word[symb_pos - 1].isnumeric()):
-                if symb_pos <= length - 2 and old_word[symb_pos + 1] == "." and old_word[symb_pos + 2] == ".":
-                    length -= 3
-                    old_word = old_word[:symb_pos] + old_word[symb_pos + 3:]
-                else:
-                    length -= 1
-                    old_word = old_word[:symb_pos] + old_word[symb_pos + 1:]
+        if i in constant.CHECK_LIST:
+            if i in constant.END_LIST:
                 end_sent = True
-            elif i == "—":
-                old_word = ""
-                symb_pos = length + 1
-            else:
-                symb_pos += 1
+
+            length -= 1
+            old_word = old_word[:symb_pos] + old_word[symb_pos + 1:]
+
+        elif abriav_checking(old_word, symb_pos):
+            length -= 1
+            old_word = old_word[:symb_pos] + old_word[symb_pos + 1:]
+            end_sent = True
+
+        else:
+            symb_pos += 1
+
     return old_word, end_sent
 
 
@@ -35,31 +40,39 @@ def clean_text(old_list):
     s = 0
     num_list = []
     start_list = []
+
     for i in old_list:
         proto_word, end_sent = clean_word(i)
+
         if end_sent:
             num_list.append(s + 1)
             s = 0
         else:
             s += 1
+
         if proto_word != "":
             start_list.append(proto_word)
+
     return start_list, num_list
 
 
 def word_count(raw_dict):
     own_dict = {}
+
     for i in raw_dict:
         if i in own_dict.keys():
             own_dict[i] += 1
         else:
             own_dict[i] = 1
-    return own_dict
 
+    return own_dict
+m_symb
 
 def symb_count(n_symb, spec_dict):
     n_dict = {}
+
     for i in spec_dict.keys():
+
         if len(i) >= n_symb:
             k = i
             n = 0
@@ -72,6 +85,7 @@ def symb_count(n_symb, spec_dict):
                     n_dict[proto] = spec_dict[i]
                 n += 1
                 m += 1
+
     return n_dict
 
 
@@ -79,12 +93,15 @@ def print_result(word_list, sent_list, symb_list, m_symb):
     print(f"Arithmetic mean of words in sentences: {int(sum(sent_list) / len(sent_list))}\n")
     print(f"Median value of words in sentences: {statistics.median(sent_list)}\n\n")
     print("\nWORD RATING:\n")
+
     for key, value in word_list.items():
         print(key, '-->', value)
+
     print("\nSYMBOL RATING:\n")
     sorted_tuples = sorted(symb_list.items(), key=lambda item: item[1], reverse=True)
     sorted_dict = {k: v for k, v in sorted_tuples}
     i = 0
+
     for key in sorted_dict:
         if i < m_symb:
             print(f"\t{key}--|--{sorted_dict[key]}")
@@ -93,15 +110,15 @@ def print_result(word_list, sent_list, symb_list, m_symb):
 
 def start_analysis(n_symb, m_symb):
     if not n_symb:
-        n_symb = constant.n_symb
+        n_symb = constant.N_SYMB
     if not m_symb:
-        m_symb = constant.m_symb
+        m_symb = constant.M_SYMB
 
     file = open("testText", "r")
     str1 = file.read()
 
-    for key in constant.replace_dict:
-        str1 = str1.replace(key, constant.replace_dict[key])
+    for key in constant.REPLACE_DICT:
+        str1 = str1.replace(key, constant.REPLACE_DICT[key])
 
     raw_list = str1.split(" ")
     new_list, sent_list = clean_text(raw_list)
