614cce6be418b99a73068e0ec30891b28d988338
Timophei Khasanau <78792353+lLeshii@users.noreply.github.com>
2022-04-29 22:08:19 +0300

    Delete constant.py

--- constant.py
+++
@@ -1,6 +0,0 @@
-REPLACE_DICT = {"\n": " ", "'ll": " will", "'m": " am", "'s": " is", "'re": " are",
-                "won't": "would not", "n't": " not", "Mr.": "Mister ", "Mrs.": "Misses ", "...": ".", "— ": ""}
-CHECK_LIST = [",", ";", "\"", "\'", ":", "(", ")", "?", "!"]
-END_LIST = [";", "?", "!"]
-N_SYMB = 4
-M_SYMB = 10
