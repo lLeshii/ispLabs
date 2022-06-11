67c5aa9a519abf9e3dbaa49b814260b7405f4b8f
Timophei Khasanau <78792353+lLeshii@users.noreply.github.com>
2022-06-11 13:47:54 +0300

    Update test_api.py

--- lab3_django/tests/test_api.py
+++ lab3_django/tests/test_api.py
@@ -36,20 +36,46 @@ class HydraTestCase(APITestCase):
         code = self.client.get(url).status_code
         self.assertEqual(status.HTTP_200_OK, code)
 
-    # def test_concrs_page_get(self):
-    #     url = reverse_lazy('concrete_category')
-    #     code = self.client.get(url).status_code
-    #     self.assertEqual(status.HTTP_200_OK, code)
+    def test_concrs_page_get(self):
+        url = reverse_lazy('concrete_category')
+        code = self.client.get(url).status_code
+        self.assertEqual(status.HTTP_200_OK, code)
+    def test_concrs_page_get(self):
+40
+        url = reverse_lazy('concrete_category')
+41
+        code = self.client.get(url).status_code
+42
+        self.assertEqual(status.HTTP_200_OK, code)
+43
+​
+44
+    def test_prod_detail_page_get(self):
+45
+        url = reverse_lazy('product_detail', args=(1,))
+46
+        code = self.client.get(url).status_code
+47
+        self.assertEqual(status.HTTP_200_OK, code)
+48
+​
+49
+    def test_cart_add_page_get(self):
+50
+        url = reverse_lazy('cart_add')
+51
+        code = self.client.get(url).status_code
+52
+        self.assertEqual(status.HTTP_200_OK, code)
+    def test_prod_detail_page_get(self):
+        url = reverse_lazy('product_detail', args=(1,))
+        code = self.client.get(url).status_code
+        self.assertEqual(status.HTTP_200_OK, code)
 
-    # def test_prod_detail_page_get(self):
-    #     url = reverse_lazy('product_detail', args=(1,))
-    #     code = self.client.get(url).status_code
-    #     self.assertEqual(status.HTTP_200_OK, code)
-
-    # def test_cart_add_page_get(self):
-    #     url = reverse_lazy('cart_add')
-    #     code = self.client.get(url).status_code
-    #     self.assertEqual(status.HTTP_200_OK, code)
+    def test_cart_add_page_get(self):
+        url = reverse_lazy('cart_add')
+        code = self.client.get(url).status_code
+        self.assertEqual(status.HTTP_200_OK, code)
 
     def test_create_product_page_get(self):
         url = reverse_lazy('create_product')
@@ -61,7 +87,7 @@ class HydraTestCase(APITestCase):
         code = self.client.get(url).status_code
         self.assertEqual(status.HTTP_200_OK, code)
 
-    # def test_index_page_get(self):
-    #     url = reverse_lazy('update_product', args=(1,))
-    #     code = self.client.get(url).status_code
-    #     self.assertEqual(status.HTTP_200_OK, code)
+    def test_index_page_get(self):
+        url = reverse_lazy('update_product', args=(1,))
+        code = self.client.get(url).status_code
+        self.assertEqual(status.HTTP_200_OK, code)
